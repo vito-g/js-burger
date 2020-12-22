b2e7a0e0206ae22ccc1f6609cbefe208b0012679
Vito Gentile <gentilevito1076@gmail.com>
2020-12-22 19:04:34 +0100

    Costo globale panino

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*Inizializzo la var "boxIngredienti" associandola al div di class ingredient-box, del DOM, che racchiude gli input, di tipo checkbox, relativi agli ingredienti. Richiamando l'elemento del DOM col class name dovrò riportare in coda all'istruzione
+/*Inizializzo la var "corpoIngredienti" associandola al div di class ingredient-box, del DOM, che racchiude gli input, di tipo checkbox, relativi agli ingredienti. Richiamando l'elemento del DOM col class name dovrò riportare in coda all'istruzione
 l'indice relativo alla classe richiamata, pur essendo, oltre che la prima (e quindi con indice pari a zero), l'unica nel DOM*/
 // var boxIngredienti = document.getElementsByClassName('ingredient-box')[0];
 //In alternativa, meglio sostituire la class con l'id?
@@ -14,10 +14,13 @@ A tal proposito inserirò una variabile (che di fatto è una costante) per il nu
 di volta in volta il numero di quelle spuntate*/
 var sogliaMinCheck = 2;
 var checkedIngredients = 0;
-// assegno una variabile relativa al costo dell' hamburger assegnando  valore di default.
-var hamCost = 4;
+// assegno una variabile relativa al costo dell' hamburger con valore di default.
+// var hamCost = 4;
 /*A questo costo andrà sommato quello di ogni singolo ingrediente aggiunto. Quindi anche il conteggio sul costo
 globale del panino andrà fatto all'interno del ciclo for*/
+
+//Inizializzo var "calcolo" associata al div del footer di id "right" ove andrò a stampare il costo finale dell'hamburger
+var calcolo = document.getElementById('right');
 
 /*SNIPPET: document.getElementById("myBtn").addEventListener("click", function() {
   alert("Hello World!");
@@ -27,13 +30,20 @@ document.getElementById('calculate').addEventListener('click', function() {
 possono esser causa di conseguenti validazioni errate. Pertanto è necessario assegnare alla variabile checkedIngredient valore pari a zero all'interno
 dell'event listener.*/
 checkedIngredients = 0;
-
+var hamCost = 4;
 for (var i = 0; i < ingredienti.length; i++) {
   if (ingredienti[i].checked) {
     checkedIngredients++;
+    hamCost += parseInt(ingredienti[i].value);
   }
+
+
+
 }
 if (checkedIngredients < sogliaMinCheck) {
   alert('devi selezionare un numero di ingredienti almeno pari a ' +  sogliaMinCheck);
+} else {
+  calcolo.innerText = hamCost;
+  console.log(hamCost);
 }
 });
