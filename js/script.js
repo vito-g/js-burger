f53fd37f0d8ac0dab9198ccd24f4a85e366c889b
Vito Gentile <gentilevito1076@gmail.com>
2020-12-22 15:31:58 +0100

    Inserito un contatore di spunte dotato di valore minimo

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ var corpoIngredienti = document.getElementById('ingredient-box');
 /*Vado ora ad assegnare una variabile che vada arichiamare gli input contenuti nella classe ingredient-box.
 Lo facco attraverso il TagName che restituisce una collection di valori(input in questo caso)*/
 var ingredienti = corpoIngredienti.getElementsByTagName('input');
+/*A questo punto potrò navigare, attraverso un ciclo for, questa collection di valori come fosse un array.
+Questo, certamente, mi torna utile nel momento del controllo sul numero minimo necessario (pari a due) di checkbox spuntate; conditio sine
+qua non, assieme alla validazione sul nome inserito dall'utente per il suo hamburger, l'evento successivo al click
+parta.
+A tal proposito inserirò una variabile contatore, per le checkbox spuntate, e la porrò uguale a 2, come di seguito*/
+var counterCheck = 2;
