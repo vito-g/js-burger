05da363dde4b061c0a28e2ead0cb62c762ae53ee
Vito Gentile <gentilevito1076@gmail.com>
2020-12-22 12:16:17 +0100

    Sostituita class con id

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-/*Inizializzo la var "boxIngredienti" associandola al div di class ingredient-box, del DOM, che racchiude gli input relativi agli ingredienti. Richiamando l'elemento del DOM col class name dovrò riportare in coda all'istruzione
+/*Inizializzo la var "boxIngredienti" associandola al div di class ingredient-box, del DOM, che racchiude gli input, di tipo checkbox, relativi agli ingredienti. Richiamando l'elemento del DOM col class name dovrò riportare in coda all'istruzione
 l'indice relativo alla classe richiamata, pur essendo, oltre che la prima (e quindi con indice pari a zero), l'unica nel DOM*/
-var boxIngredienti =document.getElementsByClassName('ingredient-box')[0];
+// var boxIngredienti =document.getElementsByClassName('ingredient-box')[0];
+//In alternativa, meglio sostituire la class con l'id?
+var boxIngredienti = document.getElementById('ingredient-box');
